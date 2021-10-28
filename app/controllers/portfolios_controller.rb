1704d7d28c07369a39f9285566acea418088c634
Anuj <mohatkaranuj@gmail.com>
2021-10-28 20:13:57 +0530

    Dropped portfolio model for scaffold to fix deprecated bugs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,24 +1,69 @@
 class PortfoliosController < ApplicationController
-	def index
-		@portfolio_items = Portfolio.all
-	end
+  before_action :set_portfolio, only: %i[ show edit update destroy ]
 
+  # GET /portfolios or /portfolios.json
+  def index
+    @portfolios = Portfolio.all
+  end
 
-	def new
-		@portfolio_item = Portfolio.new
-	end
+  # GET /portfolios/1 or /portfolios/1.json
+  def show
+  end
 
+  # GET /portfolios/new
+  def new
+    @portfolio = Portfolio.new
+  end
 
-	def create
-    	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+  # GET /portfolios/1/edit
+  def edit
+  end
 
-	    respond_to do |format|
-	      if @portfolio_item.save
-	        format.html { redirect_to portfolios_path, notice: "Blog was successfully created." }
-	      else
-	        format.html { render :new, status: :unprocessable_entity }
-	      end
-	    end
-	  end
+  # POST /portfolios or /portfolios.json
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
 
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to @portfolio, notice: "Portfolio was successfully created." }
+        format.json { render :show, status: :created, location: @portfolio }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /portfolios/1 or /portfolios/1.json
+  def update
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to @portfolio, notice: "Portfolio was successfully updated." }
+        format.json { render :show, status: :ok, location: @portfolio }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @portfolio.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /portfolios/1 or /portfolios/1.json
+  def destroy
+    @portfolio.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: "Portfolio was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    end
 end
