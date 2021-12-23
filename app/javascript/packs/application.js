03f1a5658bb42302833edb0f0116f6364c61a81a
Anuj <mohatkaranuj@gmail.com>
2021-12-23 13:13:47 +0530

    Trying to work around coffeescript, Not Successful, Leeaving for now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,16 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
 //= require jquery-ui
 //= require html.sortable
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
 
 Rails.start()
 Turbolinks.start()
